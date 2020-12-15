9d84e5c70baf9b898c0f5629ff068d17e9d86d1d
Francielma Assunção <francielma.assuncao@icen.ufpa.br>
2020-12-15 18:48:41 -0300

    Foram inseridas as guias no cadastro

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-class User{
+
+class User {
   final String cpf;
   final String nome;
   final String email;
@@ -8,6 +9,16 @@ class User{
   final String telefone;
   final String endereco;
   final String senha;
+  //inseri novos dados:
+  final String datanasc;
+  final String rg;
+  final String cep;
+  final String cidade;
+  final String uf;
+  final String numero;
+  final String bairro;
+  final String sexo;
+  final String celular;
 
   const User({
     this.cpf,
@@ -18,5 +29,15 @@ class User{
     @required this.telefone,
     @required this.endereco,
     @required this.senha,
+    //inseri novos dados:
+    @required this.datanasc,
+    @required this.rg,
+    @required this.cep,
+    @required this.cidade,
+    @required this.uf,
+    @required this.numero,
+    @required this.bairro,
+    @required this.sexo,
+    @required this.celular,
   });
 }
